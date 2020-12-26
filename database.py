f91c75f888688b88eff5b03f4f989ce7e6e5c3ff
wanjalajodham@gmail.com <wanjalajodham@gmail.com>
2020-12-26 23:50:50 +0300

    update

--- database.py
+++ database.py
@@ -18,5 +18,26 @@ def createTable(self):
  self.conn.commit()
 
  def insertData(self,data):
-     
+     insert_data="""INSERT INTO cred(username,password)
+     VALUES(?,?);"""
+     def searchData(self,data):
+         search_data = '''SELECT * FROM cred WHERE username = (?);'''
+     self.curr.execute(insert_data, data)
+
+     rows = self.curr.fetchall()
+     if rows == []:
+      return 1
+      return 0
+
+def validateData(self, data, inputData):
+         print(data)
+         print(inputData)
+
+        validate_data="""SELECT * FROM cred WHERE username= (?);"""
+        self.curr.execute(validate_data, data)
+        row = self.curr.fetchall()
+
+        if row[0][1] = inputData[0]:
+            return row[0][2] == bcrypt.hashpw(inputdata[1].encode(),row[0][2])
+
 
