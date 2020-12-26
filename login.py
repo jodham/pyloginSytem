ab2fe3664f4c23a963a709f7a543c2befacde4aa
wanjalajodham@gmail.com <wanjalajodham@gmail.com>
2020-12-26 21:51:12 +0300

    update

--- login.py
+++ login.py
@@ -3,3 +3,22 @@ from tkinter import messagebox
 import bcrypt
 from database import Database
 
+db = Database()
+db.createTable()
+
+class Login:
+    def __init__(self):
+        self.loginWindow = Tk()
+        self.loginWindow.title("Login")
+        self.loginWindow.geometry("300x250")
+        self.label = Label(self.loginWindow, text="login")
+        self.label.place(x=95, y=40)
+
+        self.usernameS = StringVar()
+        self.passwordS = StringVar()
+
+        self.usernameE = Entry(self.loginWindow, relief =FLAT, textvariable=self.usernameS)
+        self.usernameE.place(x=100, y=120)
+        self.passwordE = Entry(self.loginWindow, show="*"
+                               ,relief=FLAT,textvariable=self.passwordS)
+        self.passwordE.place(x=100, y=150)
