4b9ce4f8a49503c02f59ec7f30cbaf46b8220cfe
wanjalajodham@gmail.com <wanjalajodham@gmail.com>
2020-12-27 12:47:23 +0300

    update

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 import _sqlite3
 import bcrypt
 
+
 class Database:
     def __init__(self):
         try:
@@ -9,36 +10,36 @@ class Database:
             self.curr = self.conn.cursor()
         except:
             print("Failed")
+
     def createTable(self):
-     create_table='''CREATE TABLE IF NOT EXISTS cred(
+        create_table = '''CREATE TABLE IF NOT EXISTS cred(
             id Integer PRIMARY KEY AUTOINCREMENT,
             username TEXT NOT NULL,
             password TEXT NOT NULL);'''
-     self.curr.execute(create_table)
-     self.conn.commit()
+        self.curr.execute(create_table)
+        self.conn.commit()
 
     def insertData(self, data):
-        insert_data="""INSERT INTO cred(username,password)
+        insert_data = """INSERT INTO cred(username,password)
              VALUES(?,?);"""
         self.curr.execute(insert_data, data)
-    def searchData(self,data):
-     search_data = '''SELECT * FROM cred WHERE username = (?);'''
-     self.curr.execute(search_data, data)
 
-     rows = self.curr.fetchall()
-     if rows == []:
-        return 1
-     return 0
+    def searchData(self, data):
+        search_data = '''SELECT * FROM cred WHERE username = (?);'''
+        self.curr.execute(search_data, data)
+
+        rows = self.curr.fetchall()
+        if rows == []:
+            return 1
+        return 0
 
     def validateData(self, data, inputData):
         print(data)
         print(inputData)
 
-        validate_data="""SELECT * FROM cred WHERE username= (?);"""
-        self.curr.execute(validate_data, data)
+        validate_data = '''SELECT * FROM cred WHERE username= (?);'''
+        self.curr.execute(validate_data,(data,))
         row = self.curr.fetchall()
 
         if row[0][1] == inputData[0]:
-         return row[0][2] == bcrypt.hashpw(inputData[1].encode(),row[0][2])
-
-
+            return row[0][2] == bcrypt.hashpw(inputData[1].encode(), row[0][2])
